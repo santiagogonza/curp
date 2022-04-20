458fcabd9cbd1e9c8efc057e451afc79e2f094fb
Gonzalo <gonzalosantiago930@gmail.com>
2022-04-20 18:29:43 -0500

    avance de programa curp

---
+++ src/curp/Curp.java
@@ -0,0 +1,62 @@
+package curp;
+
+public class Curp {
+	private String nombre;
+	private String ape1;
+	private String ape2;
+	private int dia;
+	private String mes;
+	private int anio;
+	private String genen;
+	private String entidad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApe1() {
+		return ape1;
+	}
+	public void setApe1(String ape1) {
+		this.ape1 = ape1;
+	}
+	public String getApe2() {
+		return ape2;
+	}
+	public void setApe2(String ape2) {
+		this.ape2 = ape2;
+	}
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	public String getMes() {
+		return mes;
+	}
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public String getGenen() {
+		return genen;
+	}
+	public void setGenen(String genen) {
+		this.genen = genen;
+	}
+	public String getEntidad() {
+		return entidad;
+	}
+	public void setEntidad(String entidad) {
+		this.entidad = entidad;
+	}
+
+	
+}
---
+++ src/curp/main.java
@@ -0,0 +1,21 @@
+package curp;
+import java.util.Scanner;
+public class main {
+	private  static Scanner sc= new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Inserte su nombre: ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
